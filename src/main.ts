ed9d28770ef8b489d2a7a5e884cdb63dbc10547d
Dodson <aad9952@mxm-139292.local>
2018-03-17 16:14:40 -0400

    load players from database on page load

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,23 @@ import { config } from './config';
 
 import './style.scss';
 
+interface Player {
+  identifier: string;
+  element: HTMLElement;
+}
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 
-const player: HTMLElement = document.createElement('div');
-player.setAttribute('class', 'player');
-appDiv.appendChild(player);
+let players: Player[];
+let currentPlayer: Player;
 
-const player2: HTMLElement = document.createElement('div');
-player2.setAttribute('class', 'player2');
-appDiv.appendChild(player2);
-
-const players = [
-  {
-    identifier: 'player',
-    element: player
-  }, 
-  {
-    identifier: 'player2',
-    element: player2
-  }
-];
-
-let currentPlayer = players[0];
+// Makefix Constructor
+// TODO: create a main class
+function constructor() {
+  players = [];
+};
+constructor();
 
 /**
  * Key events to update player position.
@@ -85,9 +79,37 @@ try {
 // function below is also called when page loads so this funciton
 // could be removed
 firebase.database().ref().once('value').then(function (snapshot) {
-  console.log(snapshot.val());
+  addPlayers(snapshot.val());
+
+  // TODO: ask for the current player here...
+  currentPlayer = players[0];
+
   updatePlayers(snapshot.val());
 });
+
+function addPlayers(playersValue) {
+  for (var key in playersValue) {
+    // skip loop if the property is from prototype
+    if (!playersValue.hasOwnProperty(key)) continue;
+
+      // Create an HTML element for the player
+      const playerElem: HTMLElement = document.createElement('div');
+      playerElem.setAttribute('class', 'player');
+      playerElem.style.backgroundColor = getRandomHexColor();
+      playerElem.innerHTML = (players.length + 1).toString();
+      appDiv.appendChild(playerElem);
+
+      // Push the player onto the players array
+      players.push({
+        identifier: key,
+        element: playerElem
+      });
+    }
+}
+
+function getRandomHexColor() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
 
 // show the new coords on the page to demo the realtime
 // update of the database.
@@ -106,11 +128,16 @@ function updatePos(left, top) {
 }
 
 function updatePlayers(playerValues) {
-  up = playerValues[currentPlayer.identifier].top;
-  left = playerValues[currentPlayer.identifier].left;
-
-  players.forEach(player => {
-    player.element.style.top = playerValues[player.identifier].top + 'px';
-    player.element.style.left = playerValues[player.identifier].left + 'px';
-  });
+  // the on function runs on page load so it will call this function
+  // adding the players.length so that this code won't run unless
+  // the players array is initialized
+  if (players.length) {
+    up = playerValues[currentPlayer.identifier].top;
+    left = playerValues[currentPlayer.identifier].left;
+  
+    players.forEach(player => {
+      player.element.style.top = playerValues[player.identifier].top + 'px';
+      player.element.style.left = playerValues[player.identifier].left + 'px';
+    });
+  }
 }
